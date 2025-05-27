96aef88d5e0980e5e18ec098100f5cb36cb23d5f
MarcoBisson <marc-olivier.bisson.1@ens.etsmtl.ca>
2025-05-27 13:33:19 +0000

    Remove projects

--- backend/prisma/migrations/20250526134213_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Product" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "quantity" INTEGER NOT NULL,
-
-    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
-);
